4842a7b1e5e4c45c994d0929b4f6d801eae75724
Mohd Yunus <saifjava2@gmail.com>
2024-07-16 20:58:54 +0530

    all needed components and dependecies installed

---
+++ src/app/workout-form/workout-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-form.component.html',
+  styleUrl: './workout-form.component.css'
+})
+export class WorkoutFormComponent {
+
+}
